f40571180d8ab64e44adb823ff5dcfdc87d1b6c1
Oliver Metz <softwaredevdad@gmail.com>
2026-05-11 13:41:34 +0200

    new: Initial Project.
    - initial backend spring boot web structure.
    - docker compose with postgres db, pgadmin and maildev web developemnt
    mail service.
    - application.yml and -dev variant.

---
+++ src/main/java/com/devdad/book_worms/BookWormsApplication.java
@@ -0,0 +1,13 @@
+package com.devdad.book_worms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWormsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdad/book_worms/BookWormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdad.book_worms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
