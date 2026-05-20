cbcea7f1cf966e4465378667fa286cad62ca49e2
Oliver Metz <softwaredevdad@gmail.com>
2026-05-20 09:55:53 +0200

    fix: confirmationUrl property name spelling mistake.

--- src/main/java/com/devdad/book_worms/service/EmailService.java
+++ src/main/java/com/devdad/book_worms/service/EmailService.java
@@ -44,7 +44,7 @@ public class EmailService {
 
 		Map<String, Object> properties = new HashMap<>();
 		properties.put("username", username);
-		properties.put("confirmaionUrl", confirmationUrl);
+		properties.put("confirmationUrl", confirmationUrl);
 		properties.put("activation_code", activationCode);
 
 		Context context = new Context();
